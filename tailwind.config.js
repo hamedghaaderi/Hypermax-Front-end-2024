31177aa2f426184e09ddb81e7447f89ecb437497
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-09-09 23:39:11 +0330

    feat: add page title and favicon. add colors to tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,43 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      width:{
+      width: {
         "180": "180px"
       },
-      height:{
+      maxWidth: {
+        "whole": "1296px"
+      },
+      height: {
         "46": "46px"
+      },
+      fontFamily: {
+        "shabnam": "Shabnam",
+      },
+      colors: {
+        "red": "#ff3838",
+        "gray": "#777777",
+        "text": "#555555",
+        "blue": "#1494a9",
+        "white": "#ffffff",
+        "chalk": "#f5f5f5",
+        "green": "#11b76b",
+        "purple": "#b12fad",
+        "orange": "#e86121",
+        "yellow": "#ffab10",
+        "body": "#f5f6f7",
+        "border": "#e8e8e8",
+        "heading": "#39404a",
+        "primary": "#119744",
+        "sub-heading": "#565765",
+        "green-chalk": "#ddffd5",
+        "green-dark": "#072f17",
+        "gray-chalk": "#cccccc",
+        "intro-bg": "#f8fffa",
+        "facebook": "#3b5998",
+        "linkedin": "#0e76a8",
+        "twitter": "#00acee",
+        "google": "#E60023",
+        "instagram": "#F77737",
       }
     },
   },
