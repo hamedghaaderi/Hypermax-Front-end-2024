904f6afa65ebe938d65b747f5e6e825d98f85019
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-09-11 02:03:34 +0330

    feat: create mobile menu component and style it in all screens

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/header"
+import MobileMenu from "../components/mobilemenu"
 import Navbar from "../components/navbar"
 
 const HomePage = () => {
@@ -6,6 +7,7 @@ const HomePage = () => {
     <>
     <Header />
     <Navbar />
+    <MobileMenu />
     </>
   )
 }
