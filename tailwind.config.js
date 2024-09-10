f41780c9c683f7a7f5487402386142ae4bfa5a95
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-09-10 22:57:16 +0330

    chore: customize screens in tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      "tablet": "481px",
+      "desk": "769px",
+      "desklg": "1280px",
+    },
     extend: {
       width: {
         "180": "180px"
