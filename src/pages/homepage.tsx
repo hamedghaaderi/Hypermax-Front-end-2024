6771a90f6a8c13445ebde108c21037921562671d
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-09-10 13:27:55 +0330

    feat: create homepage component and render header and navbar inside it

---
+++ src/pages/homepage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header"
+import Navbar from "../components/navbar"
+
+const HomePage = () => {
+  return (
+    <>
+    <Header />
+    <Navbar />
+    </>
+  )
+}
+
+export default HomePage
