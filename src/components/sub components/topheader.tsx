fd98a07113c6493b69aeb478a2ad2d00f86f7834
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-09-12 11:12:06 +0330

    fix: fix a few styles in responsive design

--- src/components/sub components/topheader.tsx
+++ src/components/sub components/topheader.tsx
@@ -3,17 +3,17 @@ const TopHeader = () => {
     <>
       <div className="hidden desk:block bg-primary desk:py-2">
         <div className="max-w-whole m-auto desk:w-90% desklg:w-full flex flex-row-reverse items-center justify-between">
-          <p className="desk:text-xs desklg:text-sm text-white">
+          <p className="desk:text-xs desklg:text-sm cursor-default text-white">
             <span>!</span>به فروشگاه رویایی خودتان هایپرمکث، خوش آمدید
           </p>
           <div className="flex flex-row-reverse items-center justify-between">
             <div className="flex flex-row-reverse items-center justify-between ml-5">
               <i className="fa-solid fa-phone-volume desk:text-xs desklg:text-sm text-white"></i>
-              <span className="text-white desk:text-xs desklg:text-sm mr-2">021 888 9998</span>
+              <span className="text-white desk:text-xs cursor-default desklg:text-sm mr-2">021 888 9998</span>
             </div>
             <div className="flex flex-row-reverse items-center justify-between">
-              <i className="fa-regular fa-envelope desk:text-xs desklg:text-sm text-white"></i>
-              <span className="text-white desk:text-xs desklg:text-sm mr-2">support@hyper-max.ir</span>
+              <i className="fa-solid fa-envelope desk:text-xs desklg:text-sm text-white"></i>
+              <span className="text-white desk:text-xs cursor-default desklg:text-sm mr-2">support@hyper-max.ir</span>
             </div>
           </div>
         </div>
