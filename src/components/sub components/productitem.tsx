c3773ce377dea158db82c326e358d28808fbbeed
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-09-14 22:13:05 +0330

    feat: create a test object, render product item and style it.

--- src/components/sub components/productitem.tsx
+++ src/components/sub components/productitem.tsx
@@ -1,45 +1,63 @@
 import Add from "./add";
 import AddRemove from "./addremove";
 
-const ProductItem = () => {
+interface IProductItem {
+  id: number;
+  name: string;
+  price: number;
+  rate: number;
+  discount: number;
+  imageURL: string;
+}
+
+const ProductItem = ({
+  id,
+  name,
+  price,
+  rate,
+  discount,
+  imageURL,
+}: IProductItem) => {
   return (
     <>
-      <div className="p-4 h-340px relative bg-white w-60 font-shabnam border-2 border-white transition-colors duration-300 hover:border-primary hover:border-2 rounded-xl">
+      <div className="mb-12 tablet:last:mb-12 last:mb-0 p-4 h-340px relative bg-white w-60 font-shabnam border-2 border-white transition-colors duration-300 hover:border-primary hover:border-2 rounded-xl">
         <div className="h-41% w-full flex flex-row items-center justify-center">
           <img
             className="h-full w-full object-contain"
-            src="https://dkstatics-public.digikala.com/digikala-products/f7884661a211390b9a5bf928513e1c624be4e227_1709291361.jpg?x-oss-process=image/resize,w_1600/quality,q_80"
+            src={imageURL}
             alt="product picture"
           />
         </div>
         <div className="border-t border-t-border flex flex-col items-center justify-between h-59% pt-3">
           <div className="text-orange cursor-default">
-            <span className="mr-1 text-sm">3</span>
+            <span className="mr-1 text-sm">{rate}</span>
             <i className="fa-solid fa-star text-sm"></i>
           </div>
-          <div className="text-text text-center cursor-default">
-            نوشابه کانادرای قطی کولا 
-          </div>
+          <div className="text-text text-center cursor-default">{name}</div>
           <div className="flex flex-row items-center justify-center cursor-default">
             <span className="flex flex-row-reverse justify-between items-center text-primary">
-              <span>3450000</span>
+              <span>{price - (price  * discount)}</span>
               <span className="mr-1">ریال</span>
             </span>
-            <span className="flex flex-row-reverse justify-between items-center ml-2 text-red">
-              <span className="line-through text-sm">3450000</span>
-            </span>
+            {discount !== 0 && (
+              <span className="flex flex-row-reverse justify-between items-center ml-2 text-red">
+                <span className="line-through text-sm">{price}</span>
+              </span>
+            )}
           </div>
-          <AddRemove />
+          <Add />
           <div className="absolute cursor-pointer top-4 left-4 text-primary">
             <i className="fa-solid fa-heart"></i>
           </div>
           <div className="absolute cursor-pointer top-4 left-10 text-text opacity-65">
             <i className="fa-solid fa-shuffle"></i>
           </div>
-          <div className="absolute top-4 cursor-default right-4 text-xs text-white bg-orange rounded-lg px-2 py-1">
-            <span className="ml-1">تخفیف</span>
-            <span>%100</span>
-          </div>
+          {discount !== 0 && (
+            <div className="absolute top-4 cursor-default right-4 text-xs text-white bg-orange rounded-lg px-2 py-1">
+              <span className="ml-1">تخفیف</span>
+              <span>%{discount * 100}</span>
+            </div>
+          )}
         </div>
       </div>
     </>
---
+++ src/test data/fakedata.ts
@@ -0,0 +1,71 @@
+const test = {
+    "products": [
+      {
+        "id": 1,
+        "name": "ماست کاله",
+        "price": 60,
+        "rate": 5,
+        "discount": 0.2,
+        "imageURL": "https://dkstatics-public.digikala.com/digikala-products/4aaad88b3280c8ebfb91d4bb2e56c28c2e7216ec_1683963499.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_90"
+      },
+      {
+        "id": 2,
+        "name": "شیر کاکائو",
+        "price": 90,
+        "rate": 3,
+        "discount": 0.08,
+        "imageURL": "https://dkstatics-public.digikala.com/digikala-products/b205f3410722db176a374effabb95fa516349030_1709470856.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_90"
+      },
+      {
+        "id": 3,
+        "name": "نوشابه کوکا",
+        "price": 85,
+        "rate": 4,
+        "discount": 0,
+        "imageURL": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHAN3YWHffal748UOp71QpzWllcWW-zHEBJw&s"
+      },
+      {
+        "id": 4,
+        "name": "کره پگاه",
+        "price": 75,
+        "rate": 1,
+        "discount": 0.1,
+        "imageURL": "https://dkstatics-public.digikala.com/digikala-products/f7884661a211390b9a5bf928513e1c624be4e227_1709291361.jpg?x-oss-process=image/resize,w_1600/quality,q_80"
+      },
+      {
+        "id": 5,
+        "name": "ماست سنتی",
+        "price": 100,
+        "rate": 2,
+        "discount": 0.02,
+        "imageURL": "https://dkstatics-public.digikala.com/digikala-products/d513db79ade30cc23cf391283cd4c47a7b641733_1701248722.jpg?x-oss-process=image/resize,w_1600/quality,q_80"
+      },
+      {
+        "id": 6,
+        "name": "کره بادام زمینی",
+        "price": 40,
+        "rate": 4,
+        "discount": 1,
+        "imageURL": "https://dkstatics-public.digikala.com/digikala-products/110614553.jpg?x-oss-process=image/resize,w_1600/quality,q_80"
+      },
+      {
+        "id": 7,
+        "name": "توت فرنگی",
+        "price": 36,
+        "rate": 5,
+        "discount": 0.15,
+        "imageURL": "https://dkstatics-public.digikala.com/digikala-products/2697889.jpg?x-oss-process=image/resize,w_1600/quality,q_80"
+      },
+      {
+        "id": 8,
+        "name": "میوه های تابستانی مرغوب",
+        "price": 1,
+        "rate": 3,
+        "discount": 0.85,
+        "imageURL": "https://dkstatics-public.digikala.com/digikala-products/31f541f6328bd66be8dac4404a2bdab9b2689f2c_1646736060.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90"
+      }
+    ]
+  }
+
+  export default test
+  
