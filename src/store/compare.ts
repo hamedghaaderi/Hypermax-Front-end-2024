4128fb47edbd74753665d055ae2abf63aa82784e
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-09-16 01:34:16 +0330

    feat: add persist function in store to save store in local storage

--- src/store/compare.ts
+++ src/store/compare.ts
@@ -1,20 +1,32 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useCompares = create((set) => ({
-  compares: [],
-  action: {
-    addCompare: (product: any) => {
-      set((_oldCompares: any) => ({
-        compares: [..._oldCompares.compares, { ...product }],
-      }));
-    },
-    removeCompare: (product: any) => {
-      set((_oldCompares: any) => ({
-        compares: _oldCompares.compares.filter(
-          (_product: any) => _product.id !== product.id
+const useCompares = create(
+  persist(
+    (set) => ({
+      compares: [],
+      action: {
+        addCompare: (product: any) => {
+          set((_oldCompares: any) => ({
+            compares: [..._oldCompares.compares, { ...product }],
+          }));
+        },
+        removeCompare: (product: any) => {
+          set((_oldCompares: any) => ({
+            compares: _oldCompares.compares.filter(
+              (_product: any) => _product.id !== product.id
+            ),
+          }));
+        },
+      },
+    }),
+    {
+      name: "compare_items",
+      partialize: (state: any) =>
+        Object.fromEntries(
+          Object.entries(state).filter(([key]) => !["actions"].includes(key))
         ),
-      }));
-    },
-  },
-}));
+    }
+  )
+);
 export default useCompares;
