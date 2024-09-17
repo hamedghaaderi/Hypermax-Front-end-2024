369c137c8edece0c583c192fb39204c493492c18
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-09-17 13:17:17 +0330

    feat: create queryclient, config it and provide it

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60000,
+      refetchInterval: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      gcTime: 2 * 60000,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+      <ReactQueryDevtools />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
