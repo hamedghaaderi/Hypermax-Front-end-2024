9a6aa5519b5271acdcd284d24d97c19e92047fc6
Hamed Ghaderi <hamediran1182gh@gmail.com>
2024-12-13 01:50:12 +0330

    feat: add some site info into header and footer

--- src/components/sub-components/topheader.tsx
+++ src/components/sub-components/topheader.tsx
@@ -3,13 +3,14 @@ import { siteInfoContext } from "../../services/siteinfo-provider";
 
 const TopHeader = () => {
   const { info }: any = useContext(siteInfoContext);
+  console.log('info: ', info);
   
   return (
     <>
       <div className="hidden desk:block bg-primary desk:py-2">
         <div className="max-w-whole m-auto desk:w-90% desklg:w-full flex flex-row-reverse items-center justify-between">
           <p className="desk:text-xs desklg:text-sm cursor-default text-white">
-            <span>!</span>به فروشگاه رویایی خودتان هایپرمکث، خوش آمدید
+            <span>!</span>به {info?.top_slogan} خودتان هایپرمکث، خوش آمدید
           </p>
           <div className="flex flex-row-reverse items-center justify-between">
             <div className="flex flex-row-reverse items-center justify-between ml-5">
